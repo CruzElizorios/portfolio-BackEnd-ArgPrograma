92f3efbf34df4271e702e44395b442b89510c0dc
Iñaki Cruz Elizondo Rios <116087814+CruzElizorios@users.noreply.github.com>
2023-05-10 16:35:30 -0300

    primera version back

---
+++ src/main/java/com/ielizondorios/portfolioback/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.ielizondorios.portfolioback.Repository;
+
+import com.ielizondorios.portfolioback.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
---
+++ src/main/java/com/ielizondorios/portfolioback/Service/ImpPersonaService.java
@@ -0,0 +1,38 @@
+
+package com.ielizondorios.portfolioback.Service;
+
+import com.ielizondorios.portfolioback.Entity.Persona;
+import com.ielizondorios.portfolioback.Interface.IPersonaService;
+import com.ielizondorios.portfolioback.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    
+    @Override
+    public List<Persona> getPersona() {
+       List<Persona> persona = ipersonaRepository.findAll();
+       return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
