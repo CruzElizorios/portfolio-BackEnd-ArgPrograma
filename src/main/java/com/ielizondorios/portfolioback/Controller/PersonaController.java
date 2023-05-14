3be2ec880e1848c455688b28443e852dd41ab9cc
Iñaki Cruz Elizondo Rios <116087814+CruzElizorios@users.noreply.github.com>
2023-05-13 22:57:31 -0300

    mainSecurity sin errores

--- src/main/java/com/ielizondorios/portfolioback/Controller/PersonaController.java
+++ src/main/java/com/ielizondorios/portfolioback/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.ielizondorios.portfolioback.Entity.Persona;
 import com.ielizondorios.portfolioback.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,18 +27,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona) {
         ipersonaService.savePersona(persona);
         return "la persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deletePersona(id);
         return "la persona fue eliminada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}") 
     public Persona editarPersona(@PathVariable Long id,
                                  @RequestParam("nombre") String nuevoNombre,
